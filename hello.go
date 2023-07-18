20c3d457b6681109aebc2903535029db122a8459
ImaEko <nathalieewilliams94@gmail.com>
2023-07-18 22:17:54 +0100

    practice variables

---
+++ hello.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a = "sunny"
+	fmt.Println(a)
+
+	var b, c int = 2, 4
+	fmt.Println(b, c)
+
+	var d = false
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "pear"
+	fmt.Println(f)
+}
